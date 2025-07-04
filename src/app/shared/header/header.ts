ace0a6c03d92e2feae4ef92feb7607f4c7ae8240
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2025-07-04 09:32:42 +0700

    last push before ww2

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @Component({
   selector: 'app-header',
-  imports: [NzButtonModule],
+  imports: [CommonModule ,NzButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
+  isAtTop = true;
 }
